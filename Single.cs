a616a453d681c0f02e4b684df076b99bc6ea7e0e
ZhoujianhuaUnity <634023405@qq.com>
2018-11-27 15:35:55 +0800

    上传了一个单列脚本

---
+++ Single.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Single<T> : MonoBehaviour where T : Single<T>
+{
+
+    private static T instance;
+    public static T Instance
+    {
+
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectsOfType(typeof(T)) as T;
+
+            }
+            if (instance == null)
+            {
+
+                GameObject go = new GameObject();
+                instance = go.AddComponent<T>();
+
+            }
+
+            DontDestroyOnLoad(instance.gameObject);//加载场景不会销毁该物体和该脚本
+            return instance;
+
+        }
+
+
+
+    }
+}
